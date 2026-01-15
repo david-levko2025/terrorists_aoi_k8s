8948c2796ce7a8000d51ee34bddf543a1f4a0af0
david <mdavidl@gmail.com>
2026-01-15 14:15:31 +0200

    fastapi work

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File, HTTPException
 import uvicorn
 import pandas as pd
-from service import ValidationOnData as valid
+from service import Validation as valid
 from db import Connection
 
 
 app = FastAPI()
 
-
 @app.post("/top-threats")
-def get_the_csv_file(file: UploadFile):
-    df = pd.read_csv(file.file)
-    the_most_dangers_terrorists = df.sort_values(by = ["danger_rate"], ascending = False).head()
-    dangers_terrorists_valid = valid.validating_terrorists(the_most_dangers_terrorists)
-    result =Connection.insert_to_db(dangers_terrorists_valid)
-    return {"count": len(dangers_terrorists_valid),
-            "top": dangers_terrorists_valid}
+def create_file(file: UploadFile = File(...)):
+    if not file:
+        raise HTTPException(detail="file not provided",status_code=400)
+    top_terorists = valid(f'{file.filename}').top_terorist().to_dict(orient="dict")
+    
+    return { 
+        "count":len(top_terorists),
+         "top": [top_terorists] 
+         }
 
 
 if __name__ == "__main__":
     uvicorn.run(app,host="localhost",port=8000)
+
+    # df = pd.read_csv(file.filename)
+    # # the_most_dangers_terrorists = df.sort_values(by = ["danger_rate"], ascending = False).head()
+    # dangers_terrorists_valid = valid.validation_data()
+    # result =Connection.insert_to_db(dangers_terrorists_valid)
+    # return {"count": len(dangers_terrorists_valid),
+    #         "top": dangers_terrorists_valid}
--- service.py
+++ service.py
@@ -1,18 +1,35 @@
 from models import Terorists
 import pandas as pd
 
-class ValidationOnData:
 
-    @staticmethod
-    def validating_terrorists(top_5_dangers):
-        terrorists_list = []
-        for terrorists in top_5_dangers:
-            terrorists_list.append(Terorists(terrorists["name"] ,
-                                             terrorists["location"] ,
-                                             terrorists["danger_rate"]).__dict__())
-        
-        return terrorists_list
-    
-    def insert_to_mongo(df):
-        pass
+class Validation():
+    def __init__(self, file) -> None:
+        self.file = pd.read_csv(file)
+
+    def top_terorist(self):
+        self.file.sort_values(by="danger_rate", ascending=False)
+        top_terorist = self.file.head()
+        return top_terorist   
+
+    def drop_column_age(self):
+        self.file.drop('age',axis=1)
+        drop_column = self.file.head()
+        return drop_column
+
+    def drop_column_group(self):    
+        self.file.drop('group',axis=1)
+        drop_column = self.file.head()
+        return drop_column
+
+    def drop_column_last_seen(self):
+        self.file.drop('last_seen',axis=1)
+        drop_column = self.file.head()
+        return drop_column
+
+    def drop_column_notes(self):    
+        self.file.drop('notes',axis=1)
+        drop_column = self.file.head()
+        return drop_column
+
+
 
