622b351cb31084208cd38ba388873267e675f15c
david-levko2025 <mdavidl2003@gmail.com>
2026-01-15 15:06:14 +0200

    Remove commented-out code from main.py

--- main.py
+++ main.py
@@ -21,10 +21,3 @@ def create_file(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     uvicorn.run(app,host="localhost",port=8000)
-
-    # df = pd.read_csv(file.filename)
-    # # the_most_dangers_terrorists = df.sort_values(by = ["danger_rate"], ascending = False).head()
-    # dangers_terrorists_valid = valid.validation_data()
-    # result =Connection.insert_to_db(dangers_terrorists_valid)
-    # return {"count": len(dangers_terrorists_valid),
-    #         "top": dangers_terrorists_valid}
